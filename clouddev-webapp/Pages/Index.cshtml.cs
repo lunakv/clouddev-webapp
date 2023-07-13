f59c98a13555dd9dc7a3339bd632400bf061c376
pc <pc@DESKTOP-DKGAKN8>
2023-07-13 11:23:21 -0700

    Add project files.

---
+++ clouddev-webapp/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace clouddev_webapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
+
+        public List<BlobItem>? Blobs { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+
+            Configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            Blobs = CreateClient().GetBlobs().ToList();
+        }
+
+        public FileStreamResult OnGetDownloadBlob(string name)
+        {
+            var client = CreateClient().GetBlobClient(name);
+            var contentType = client.GetProperties().Value.ContentType;
+            var stream = client.OpenRead();
+            return new FileStreamResult(stream, contentType); 
+        }
+
+        private BlobContainerClient CreateClient()
+        {
+            string connectionString = Configuration.GetConnectionString("BlobStorage");
+            string containerName = Configuration.GetValue<string>("StorageContainerName");
+            return new BlobContainerClient(connectionString, containerName);
+        }
+    }
+}
